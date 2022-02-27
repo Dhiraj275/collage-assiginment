5dee194298f14bf7980be4ab61462cfec8865618
Dhruv Prajapati <rp170854@gmail.com>
2022-02-27 21:17:10 +0530

    first commit

---
+++ Python/compair_module.py
@@ -0,0 +1,8 @@
+test_list=[4,4,5,4,5,45,45,4545,54,4,5]
+flag=False
+for i in test_list:
+    if(i==9):
+        print('ye boi')
+        flag= True
+if(flag==False):
+    print('no boi')
---
+++ Python/messageSpamingBot.py
@@ -0,0 +1,11 @@
+from time import sleep
+import pyautogui as pg
+# sleep(10)
+txt= open('E:\Collage Work\Programming\Python/animals.txt', 'r')
+# pg.write('All the messages sent after this are sent by a bot made by *Dhiraj* Please dont take it seriously ')
+# pg.press('Enter')
+# a = 'Anisha is a '
+# for i in txt:
+#   pg.write(a+i)
+#   pg.press('Enter')
+print(txt)
